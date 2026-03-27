a399041b867df39c102c17efa33d5354cad77869
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2026-03-27 17:04:52 +0300

    [utils] Move make_uuid variant stamp out of if blk
    Signed-off-by: Mustafa Kemal Gilor <mustafa.gilor@canonical.com>

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -378,7 +378,6 @@ std::string mp::utils::make_uuid(const std::optional<std::string>& seed)
             throw std::runtime_error{"MD5 digest failed"};
 
         bytes[6] = (bytes[6] & 0x0Fu) | 0x30u; // version 3
-        bytes[8] = (bytes[8] & 0x3Fu) | 0x80u; // RFC 4122 variant
     }
     else
     {
@@ -387,8 +386,8 @@ std::string mp::utils::make_uuid(const std::optional<std::string>& seed)
             throw std::runtime_error{"RAND_bytes failed"};
 
         bytes[6] = (bytes[6] & 0x0Fu) | 0x40u; // version 4
-        bytes[8] = (bytes[8] & 0x3Fu) | 0x80u; // RFC 4122 variant
     }
+    bytes[8] = (bytes[8] & 0x3Fu) | 0x80u; // RFC 4122 variant
 
     return fmt::format("{:02x}{:02x}{:02x}{:02x}-{:02x}{:02x}-{:02x}{:02x}-{:02x}{:02x}-{:02x}{:"
                        "02x}{:02x}{:02x}{:02x}{:02x}",
